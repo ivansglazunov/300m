9c8c6c14c89889f34af092c522e1658b1731b997
Ivan S Glazunov <ivansglazunov@gmail.com>
2019-06-08 19:56:32 +0300

    bit on commit

---
+++ dist/src/components/example/index.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+declare const _default: ({ currentUser, setCurrentUser, count, setCount }: {
+    currentUser: any;
+    setCurrentUser: any;
+    count: any;
+    setCount: any;
+}) => JSX.Element;
+export default _default;
